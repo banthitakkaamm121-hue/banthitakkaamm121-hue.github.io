87777169b1c8e9809d109337db6ddd54af2b8ca1
banthitakkaamm121-hue <banthitakkaamm121@gmail.com>
2026-08-11 09:33:51 +0700

    Fix syntax errors and update comments in script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document .addEventListener('
+// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
+document.addEventListener('DOMContentLoaded',function () {
+   console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+}};
